2f2a2081ba417a3876855610bde3523de71314fa
Tingting Xu <txu@scu.edu>
2015-11-04 16:36:59 -0800

    added signup page

--- event.js
+++ event.js
@@ -2,3 +2,18 @@ function getDate(){
   var x = new Date(document.lastModified);
     document.getElementById("lastModified").innerHTML = x;
 }
+
+function changeImage(){
+	var button = document.getElementById("id");
+	document.getElementById("imageChange").src = "img2.jpg";
+}
+
+function signin(){
+	var text = document.getElementById("sign");
+	if(text.innerHTML == "Sign In"){
+		text.innerHTML = "Sign Up";
+	}
+	else if(text.innerHTML == "Sign Up"){
+		text.innerHTML = "Sign In";
+	}
+}

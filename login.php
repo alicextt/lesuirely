d89c1c0a9c69e24edba1474b945ae6f217f25116
Tingting Xu <txu@scu.edu>
2015-11-11 16:58:55 -0800

    added footer, rename files, fix login initial erro

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>
-    Sign Up
+    Log In
   </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +15,7 @@
   <link href="styles.css" rel="stylesheet">
 </head>
 
-<?php 
+<?php
 error_reporting(0);
 $email_val="";
 $anemail_val="";
@@ -23,12 +23,12 @@ $anemail_val="";
 $done=0;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	//echo $_POST["visitorname"];
-		
+
 		$email = $_POST["visitoremail"];
 		$anemail = $_POST["anothervisitoremail"];
-	
-		
-		
+
+
+
 		if($email==""){
 		$email_val="*No Email address entered";
 		}else{
@@ -47,66 +47,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		if($anemail==""){
 		$anemail_val="*NO Password entered";
 		}else{
-	if($okay1 =ctype_alnum($anemail) // numbers & digits only 
-        && strlen($anemail)>6 // at least 7 chars 
-        && strlen($anemail)<21 // at most 20 chars 
-        && preg_match('`[A-Z]`',$anemail) // at least one upper case 
-        && preg_match('`[a-z]`',$anemail) // at least one lower case 
-        && preg_match('`[0-9]`',$anemail) // at least one digit 
+	if($okay1 =ctype_alnum($anemail) // numbers & digits only
+        && strlen($anemail)>6 // at least 7 chars
+        && strlen($anemail)<21 // at most 20 chars
+        && preg_match('`[A-Z]`',$anemail) // at least one upper case
+        && preg_match('`[a-z]`',$anemail) // at least one lower case
+        && preg_match('`[0-9]`',$anemail) // at least one digit
         )
  {
 			$done3=1;
      }
 else{
   $anemail_val="*password for length between(6,21),at least     contains  1-uppercase, 1-lowercase and 1-numbers.";
-		
+
 		$done=0;
 		}
-		
+
 		}
-		
 
 
-		 
+
+
 }
 
 
 
 ?>
- ?>
 
 <body>
   <!-- header-->
-  <noscript> Browser does not support JAVASCRIPT</noscript>
-  <header class="container">
-    <div class="horizontal">
-      <ul>
-        <li><a href="homepage.html"><img src="logo.png" alt="leisurely" height="110"
-          width="125"></li></a>
-          <li><a href="moviePage.html">Movies</a></li>
-          <li><a href="#">Books</a></li>
-          <li>
-            <form accept-charset="utf-8" class="nav-search" method="GET" name="site-search" action="search.php">
-              <div>
-                <button class="glyphicon glyphicon-search" id="nav-search-con" type="Submit"/>
-              </div>
-              <div class="nav-fill">
-                <input placeholder="Search" autocomplete="off" name="search" type="text">
-              </div>
-            </form>
-          </li>
-         <li><a id = "signup" href = "signUp.html">Join Today</a></li>
-          <li><a id = "sign" href = "login.php">Sign In</a></li>
-          <li><a href=""><img src="cart.png" alt="Lesuirely" height="50" width="50"></a></li>
-        </ul>
-      </div>
-    </header>
+<noscript> Browser does not support JAVASCRIPT</noscript>
+<header class="container">
+  <div class="horizontal">
+    <ul>
+      <li><a href="index.html"><img src="logo.png" alt="leisurely" height="110"
+        width="125"></li></a>
+        <li><a href="movie.html">Movies</a></li>
+        <li><a href="#">Books</a></li>
+        <li>
+          <form accept-charset="utf-8" class="nav-search" method="GET" name="site-search" action="search.php">
+            <div>
+              <button class="glyphicon glyphicon-search" id="nav-search-con" type="Submit"/>
+            </div>
+            <div class="nav-fill">
+              <input placeholder="Search" autocomplete="off" name="search" type="text">
+            </div>
+          </form>
+        </li>
+        <li><a id = "signup" href = "signUp.html">Join Today</a></li>
+        <li><a id = "sign" href="login.php">Sign In</a></li>
+        <li><a href="checkout.html"><img src="cart.png" alt="Lesuirely" height="50" width="50"></a></li>
+      </ul>
+    </div>
+  </header>
     <div class="right">
-      <center><legend>Login Now!</legend></center>
-    
+    <legend>Login Now!</legend>
+
 
 <?php echo $image_uploaded;?>
-<form action = "index.php" method= "POST" name="form1" id="form1" enctype="multipart/form-data"> 
+<form action = "index.php" method= "POST" name="form1" id="form1" enctype="multipart/form-data">
 <input type="hidden" name="to" value="YOUREMAIL@YOURDOMAIN.COM">
 <input type="hidden" name="from" value="YOUREMAIL@YOURDOMAIN.COM">
 <input type="hidden" name="subject" value="EMAIL SUBJECT LINE">
@@ -114,8 +113,6 @@ else{
 <input type="hidden" name="notefields" value="Name:~~visitorname~~<BR><BR>E-Mail:~~visitoremail~~<BR><BR>Comments:~~comments~~<BR>">
 
 
-
-          
           <div class="form-group">
             <label for="email">Email:</label>
             <input type="text" id="email"
@@ -126,13 +123,19 @@ name="visitoremail" value="<?php echo $email;?>"><?php if($email_val){echo $emai
             <label for="pwd">Password:</label>
             <input type="password" class="form-control" id="pwd" name="anothervisitoremail" value="<?php echo $anemail;?>"><?php if($anemail_val){echo $anemail_val;}?><div class="error" id="pwderror"></div>
           </div>
-          
+
           <div class="center">
         <button class="btn btn-warning" type="button" id="submit">Submit</button>
       </div>
     </form>
 </div>
+<div id="copycont">
+  <footer>
+      <p class="center"> Copyright@2015, designed by <a class="yellow">Leisurely Admin | Privacy Policy</a></p>
+      <p class="center">  Site Last Modified:
+        <span id="lastModified"/> </p>
+        <noscript> Browser does not support JAVASCRIPT</noscript>
+      </footer>
+</div>
 </body>
-
-
 </html>

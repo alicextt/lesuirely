8d8f2a4e9458c600769860cf82a29b6982796edd
Ting ting Xu <alice.xtt@gmail.com>
2015-11-28 15:54:58 -0800

    add function to restore user session info

---
+++ db_data/add_sessions.sql
@@ -0,0 +1,52 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4135
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.5.42)
+# Database: Leisurely
+# Generation Time: 2015-11-28 23:54:04 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sessions`;
+
+CREATE TABLE `sessions` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `access` int(10) NOT NULL,
+  `data` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+
+INSERT INTO `sessions` (`id`, `access`, `data`)
+VALUES
+	(3,1448753903,'YToxOntpOjA7Tzo1OiJwaXRlbSI6Nzp7czo1OiJwdHlwZSI7czo0OiJSZW50IjtzOjU6ImN0eXBlIjtzOjEyOiIKICAgIG1vdmllICAiO3M6NjoidXByaWNlIjtzOjY6IiQgNS4zNSI7czo4OiJxdWFudGl0eSI7czoxOiIxIjtzOjU6InRpdGxlIjtzOjI3OiJHLkkuIEpvZTogVGhlIFJpc2Ugb2YgQ29icmEiO3M6MzoiaW1nIjtzOjEwMDoiaHR0cDovL2lhLm1lZGlhLWltZGIuY29tL2ltYWdlcy9NL01WNUJNVFF6TVRVMU56UXdObDVCTWw1QmFuQm5Ya0Z0WlRjd05EZzROek16TXdAQC5fVjFfU1gyMTRfQUxfLmpwZyI7czoyOiJpZCI7czoxOiIyIjt9fQ==');
+
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
